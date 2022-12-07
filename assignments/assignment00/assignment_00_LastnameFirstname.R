5f7663c3c5983e7a5c66a78fd99b7959f7488c56
BackwardToast96 <112522796+BackwardToast96@users.noreply.github.com>
2022-12-06 20:36:57 -0500

    Update assignment_00_LastnameFirstname.R

--- assignments/assignment00/assignment_00_LastnameFirstname.R
+++ assignments/assignment00/assignment_00_LastnameFirstname.R
@@ -5,48 +5,58 @@
 # Basics
 
 ## Add 8 and 5
-___
+a<-8
+b<-5
+c<-a+b
 
 ## Subtract 6 from 22
-___
+a<-22
+b<-6
+c<-a-b
 
 ## Multiply 6 by 7
-___
+a<-7
+b<-6
+c<-a*b
 
 ## Add 4 to 6 and divide the result by 2
-___
+a<-4
+b<-6
+c<-(a+b)/2)
 
 ## Compute 5 modulo 2
-___
+a<-5%%2
 
 ## Assign the value 82 to the variable x
 ## Print x
-x <- ___
-x
+x <- 82
+print(x)
 
 ## Assign the value 41 to the variable y
 ## Print y
-y <- ___
-___
+y <- 41
+print(y)
 
 ## Assign the output of x + y to the variable z
 ## Print z
-z <- ___
-___
+z <- x+y
+print(z)
 
 ## Assign the string value "DSC520" to the variable class_name
 ## Print the value of class_name
-class_name <- ___
+class_name <- "DSC520"
+print(class_name)
 
 ## Assign the string value of TRUE to the variable is_good
 ## Print the value of is_good
-is_good <- ___
+is_good <- TRUE
+print(is_good)
 
 ## Check the class of the variable is_good using the `class()` function
-___
+class(is_good)
 
 ## Check the class of the variable z using the `class()` function
-___
+class(z)
 
 ## Check the class of the variable class_name using the class() function
-___
+class(class_name)
